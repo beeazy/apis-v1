2552311d4bfca9600b19c531e11e7b4985e914a9
beeazy <mutwirievans95@gmail.com>
2024-11-26 21:22:35 +0300

    get request

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
@@ -14,6 +15,8 @@ class TaskController extends Controller
     public function index()
     {
         //
+
+        return Task::all();
     }
 
     /**
